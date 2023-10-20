a6d5f64779bc53d69796efd5ccad45e51e64b240
Staysecret <114147003+MarvinWaro@users.noreply.github.com>
2023-10-21 07:47:50 +0800

    OPENAI API Integrated

--- reach/reach/settings.py
+++ reach/reach/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+#GPT_API_SECRET_KEY
+OPENAI_KEY = config('OPENAI_KEY')
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hk5*8h%$f!!tkguz2y)e#d3tarjyscjws%%bs(548m2i24uau%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
