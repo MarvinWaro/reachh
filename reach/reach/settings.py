a621803e626c6044cee311416cd498dd41905b15
Seizaku.dev <zakusei.dev@gmail.com>
2024-03-10 16:06:32 +0800

    Update settings.py

--- reach/reach/settings.py
+++ reach/reach/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'https://reachh.onrender.com/static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
