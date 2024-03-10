914feb41de77361842dde81d645340133c9370f0
Seizaku.dev <zakusei.dev@gmail.com>
2024-03-10 15:16:49 +0800

    Update settings.py site root

--- reach/reach/settings.py
+++ reach/reach/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/var/www/reachh.onrender.com/static/"
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
